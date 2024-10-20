e38d9f5a98b0033f015a8419a8cf09679d0dc218
Podexus23 <santon23@gmail.com>
2024-10-20 14:30:51 +0300

    feat: add get method

--- src/controller/url.controller.ts
+++ src/controller/url.controller.ts
@@ -1,5 +1,6 @@
 import { IncomingMessage, ServerResponse } from 'http';
 import sendResponse from '../helpers/response.helper';
+import { getUser, getUsers } from '../methods/get.method';
 
 const LINKS = {
   main: '/api/users',
@@ -9,9 +10,15 @@ export default function urlController(req: IncomingMessage, res: ServerResponse)
   try {
     const address = req.url;
     const fixedAddress = address?.replace('$', '');
+    const id = fixedAddress?.split('/')[3];
 
     if (!fixedAddress?.includes(LINKS.main) || address === '/api/users/') {
       sendResponse(res, 404, 'Sorry. Page not found');
+    }
+
+    if (req.method === 'GET') {
+      if (fixedAddress === LINKS.main) getUsers(res);
+      if (fixedAddress?.includes(LINKS.main) && id) getUser(res, id);
     }
   } catch {
     sendResponse(
