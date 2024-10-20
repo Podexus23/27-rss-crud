0edd04a46c696ff70e63e0e2a515d6dbfc4db83e
Podexus23 <santon23@gmail.com>
2024-10-20 14:09:53 +0300

    feat: add logger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import http from 'http';
 import * as dotenv from 'dotenv';
+import requestLog from './helpers/logger.helpers';
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer();
+const server = http.createServer((req) => {
+  try {
+    requestLog(req);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 server.listen(port);
 console.log(`Server is working on Port: ${port}`);
